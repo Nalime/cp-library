ca15c02517fc4c160e61a8bccfc2165a035325f5
Landy <tw.landy.lu@gmail.com>
2021-05-09 21:41:01 +0800

    point.cpp reordering

--- geo/point.cpp
+++ geo/point.cpp
@@ -7,10 +7,21 @@ struct point {
 
     explicit operator pair<T, T>() const { return {x, y}; }
 
+    point operator+() const { return *this; }
+    point operator-() const { return point(-x, -y); }
+
+    // be careful of overflow
+    T norm() { return x * x + y * y; }
+    F abs() { return sqrtl(norm()); }
+
     point& operator+=(const point &o) {
         x += o.x;
         y += o.y;
         return *this;
+    }
+
+    friend point operator+(const point &s, const point &t) {
+        return point(s) += t;
     }
 
     point& operator-=(const point &o) {
@@ -19,22 +30,16 @@ struct point {
         return *this;
     }
 
+    friend point operator-(const point &s, const point &t) {
+        return point(s) -= t;
+    }
+
     // scalar product
+
     point& operator*=(T s) {
         x *= s;
         y *= s;
         return *this;
-    }
-
-    point operator+() const { return *this; }
-    point operator-() const { return point(-x, -y); }
-
-    friend point operator+(const point &s, const point &t) {
-        return point(s) += t;
-    }
-
-    friend point operator-(const point &s, const point &t) {
-        return point(s) -= t;
     }
 
     friend point operator*(T s, const point &t) {
@@ -44,6 +49,18 @@ struct point {
     point operator*(T s) const {
         return point(*this) *= s;
     }
+
+    // dot product
+    friend T operator^(const point &s, const point &t) {
+        return s.x * t.x + s.y * t.y;
+    }
+
+    // cross product
+    friend T operator%(const point &s, const point &t) {
+        return s.x * t.y - s.y * t.x;
+    }
+
+    // pair-like comparision
 
     friend bool operator==(const point &s, const point &t) {
         return pair<T, T>(s) == pair<T, T>(t);
@@ -69,19 +86,7 @@ struct point {
         return pair<T, T>(s) >= pair<T, T>(t);
     }
 
-    // dot product
-    friend T operator^(const point &s, const point &t) {
-        return s.x * t.x + s.y * t.y;
-    }
-
-    // cross product
-    friend T operator%(const point &s, const point &t) {
-        return s.x * t.y - s.y * t.x;
-    }
-
-    // be careful of overflow
-    T norm() { return x * x + y * y; }
-    F abs() { return sqrtl(norm()); }
+    // streams
 
     friend istream& operator>>(istream &is, point &t) {
         T p, q;
